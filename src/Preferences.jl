ebab4e2c5ed34b6eecb8eb679f4cdcfd946b4a0f
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-05-09 15:47:12 +0200

    add Julia compat note and switch order for clearer error

--- src/Preferences.jl
+++ src/Preferences.jl
@@ -1,11 +1,11 @@
 module Preferences
 
-using TOML
-using Base: UUID, TOMLCache
-
 if VERSION < v"1.6.0-DEV"
     error("Preferences.jl can only be used on Julia v1.6+!")
 end
+
+using TOML
+using Base: UUID, TOMLCache
 
 export load_preference, @load_preference,
        has_preference, @has_preference,
