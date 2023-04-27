582f194a397659d4c4b3412641b1132f553f94aa
Shuhei Kadowaki <aviatesk@gmail.com>
2023-04-27 19:17:38 +0900

    provide a way to allow Preferences.jl to work on non-package modules
    The aim of this PR is to provide a way to use Preferences.jl for modules
    that are defined at runtime. It is useful for for debugging or analysis
    purposes.
    
    When we want to debug a package code, we may want to run it as top-level
    script, e.g. <https://github.com/JuliaLang/PrecompileTools.jl/issues/11>
    ```julia
    julia> using PrecompileTools
    
    julia> PrecompileTools.verbose[] = true
    
    julia> include("src/XXX.jl") # run XXX.jl as a script to see what gets precompiled
    ERROR: LoadError: ArgumentError: Module Main.XXX does not correspond to a loaded package!
    Stacktrace:
     [1] get_uuid(m::Module)
       @ Preferences ~/.julia/packages/Preferences/VmJXL/src/utils.jl:8
     ...
    ```
    
    Currently this does not work since `get_uuid(m)` throws when `m` is not
    a proper package (in cases like above, `m` is a submodule of `Main`).
    
    With this commit, we can use `Preferences.uuid_cache` to temporarily
    make it work:
    ```julia
    julia> using Preferences, Pkg
    
    julia> Preferences.uuid_cache[Main] =
               Pkg.project().dependencies["XXX"]
    
    julia> include("src/XXX.jl")
    ```
    
    This is also useful for analysis packages like JET.jl, which analyzes
    a package by running its code in the `Main` module.

--- src/utils.jl
+++ src/utils.jl
@@ -1,13 +1,39 @@
 # Helper function to detect if we're currently compiling
 currently_compiling() = ccall(:jl_generating_output, Cint, ()) != 0
 
+"""
+    Preferences.uuid_cache::Dict{Module,UUID}
+
+Runtime cache to store the UUIDs of root modules.
+Users can make Preferences.jl to work for non-package modules by manually adding a proper
+entry to this cache. It may be useful for debugging or analysis purposes.
+
+```julia
+julia> using Preferences, Pkg
+
+julia> Preferences.uuid_cache[Main] =
+           Pkg.project().dependencies["XXX"]
+
+julia> include("src/XXX.jl") # `Main.XXX` can load configurations for XXX.jl
+```
+
+!!! warning
+    Improper manipulation on this cache may cause unexpected behaviors.
+    Use with care and only as a last resort if absolutely required.
+"""
+const uuid_cache = Dict{Module,UUID}()
+
 # Helper function to get the UUID of a module, throwing an error if it can't.
 function get_uuid(m::Module)
+    rootm = Base.moduleroot(m)
+    if haskey(uuid_cache, rootm)
+        return uuid_cache[rootm]
+    end
     uuid = Base.PkgId(m).uuid
     if uuid === nothing
         throw(ArgumentError("Module $(m) does not correspond to a loaded package!"))
     end
-    return uuid
+    return uuid_cache[rootm] = uuid
 end
 
 function find_first_project_with_uuid(uuid::UUID)
